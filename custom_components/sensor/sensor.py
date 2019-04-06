447d59b40a818e4b334b58d30f4a5b2b6dd2cd80
Toast <swetoast@users.noreply.github.com>
2019-04-06 19:54:05 +0200

    Update sensor.py

--- custom_components/sensor/sensor.py
+++ custom_components/sensor/sensor.py
@@ -8,7 +8,7 @@ from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import (PLATFORM_SCHEMA)
 
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 
 _LOGGER = logging.getLogger(__name__)
 
